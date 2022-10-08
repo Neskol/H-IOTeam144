53a38497dc59d7d06de3aa2eaa70b75dc18ee80b
Neskol Lu <hs-neskol@outlook.com>
2022-10-08 14:56:12 -0400

    NAudio

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using NAudio;
+using NAudio.Wave;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Mp3FileReader reader = new Mp3FileReader("/Users/Neskol/MaiAnalysis/Sound/music000834.mp3");
+
 

6ddfb6f951188c4fdf822c8f732a38d9acb4dc5c
Neskol <hs-neskol@outlook.com>
2022-10-08 13:05:53 -0400

    Updates

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Collections;
+using System.Collections.Generic;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
